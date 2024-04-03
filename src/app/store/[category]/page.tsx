e70b6ecc4eea4ce183560cc7926298a286b1880e
Abrajam7611 <luis.vel4@gmail.com>
2024-04-03 16:23:16 -0600

    1.5 Como crear Layout en next

--- src/app/store/[category]/page.tsx
+++ src/app/store/[category]/page.tsx
@@ -7,6 +7,6 @@ interface CategoryProps {
 export default function Category(props: CategoryProps){
     const { category } = props.params
     return(
-        <h1>Categoría dinámica {category}</h1>
+        <h1>Categoría dinámica: {category}</h1>
     );
 }
