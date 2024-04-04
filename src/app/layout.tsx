bf6ef03482628578697a423755aae3deb20abee4
Abrajam7611 <luis.vel4@gmail.com>
2024-04-03 23:01:38 -0600

    1.9 Creación de arquitectura de Landing Page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,20 @@
-"use client"
-import { Header } from 'app/components/Header';
 import { Inter } from "next/font/google";
-import "./globals.css";
+import { Header } from "app/components/shared/Header";
+import { Footer } from "app/components/shared/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  console.log('Hola mundo layout')
+  children,}: Readonly<{
+  children: React.ReactNode;}>) {
+
   return (
     <html lang="en">
-      <Header />
       <body className={inter.className}>
-      
-        {children}</body>
+      <Header/> 
+        {children}
+        <Footer/>
+        </body>
     </html>
   );
 }
---
+++ src/components/home/Description/Description.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const Description = ()=>{
+    return (
+        <section>
+            <h1>Descripción</h1>
+        </section>
+    )
+}
