7f0b0ee8e2c92c35de999975f4e5062c87192530
Abrajam7611 <luis.vel4x@gmail.com>
2024-04-19 22:31:28 -0600

    4.7 Patrones data fetching

--- src/services/shopify/products.ts
+++ src/services/shopify/products.ts
@@ -1,10 +1,9 @@
-import { env } from "app/config/env"
+import { env } from "process";
 
 export const shopifyUrls ={
     products:{
         'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`
-    }
-
-},
-    collections: {'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/smart_collections.json`
-}
+    },
+    collections:
+            {'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/smart_collections.json`
+}}
